00160fec228258af4b236124cbbbad6617928b5c
Wendy Smoak <wsmoak@gmail.com>
2015-12-20 18:25:29 -0500

    Add Guardian to pipelines

---
+++ lib/secret_keeper/GuardianSerializer.ex
@@ -0,0 +1,14 @@
+# taken from https://github.com/ueberauth/guardian
+
+defmodule SecretKeeper.GuardianSerializer do
+  @behaviour Guardian.Serializer
+
+  alias SecretKeeper.Repo
+  alias SecretKeeper.User
+
+  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
+  def for_token(_), do: { :error, "Unknown resource type" }
+
+  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
+  def from_token(_), do: { :error, "Unknown resource type" }
+end
--- web/router.ex
+++ web/router.ex
@@ -9,19 +9,26 @@ defmodule SecretKeeper.Router do
     plug :put_secure_browser_headers
   end
 
+  pipeline :browser_session do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
   end
 
   scope "/", SecretKeeper do
-    pipe_through :browser # Use the default browser stack
+    pipe_through [:browser, :browser_session] # Use the default browser stack
 
     get "/", PageController, :index
     resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", SecretKeeper do
-  #   pipe_through :api
-  # end
+   scope "/api", SecretKeeper do
+     pipe_through :api
+   end
 end
