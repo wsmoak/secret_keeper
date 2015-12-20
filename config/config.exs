32e18c5fce8104ba43f09f395e8526be495f45e6
Wendy Smoak <wsmoak@gmail.com>
2015-12-20 17:39:25 -0500

    Add Guardian and its config

--- config/config.exs
+++ config/config.exs
@@ -27,3 +27,12 @@ import_config "#{Mix.env}.exs"
 config :phoenix, :generators,
   migration: true,
   binary_id: false
+
+config :guardian, Guardian,
+  allowed_algos: ["HS512"], # optional
+  verify_module: Guardian.JWT,  # optional
+  issuer: "SecretKeeper",
+  ttl: { 30, :days },
+  verify_issuer: true, # optional
+  secret_key: "5TDVCSTHt+X/uJNr0fV7QKJTlCVS//3Dxudqib3xrrriRF7wUgne5AyD1BT2LCKG",
+  serializer: SecretKeeper.GuardianSerializer
--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule SecretKeeper.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 2.0"}]
+     {:comeonin, "~> 2.0"},
+     {:guardian, "~> 0.8.0"},
+   ]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
